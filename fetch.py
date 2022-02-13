1dc67072985e2e2dd5befc9e41d5804b6753e45e
yxing11 <yxing11@usfca.edu>
2022-02-13 02:54:04 -0800

    tweak vars

--- fetch.py
+++ fetch.py
@@ -3,9 +3,19 @@ from credential_pool import keys
 
 
 def main():
+    keyword = "btc"
+    N = 500
+    isTimeRange = False
+    # The following time range must be given if `isTimeRange` is set to True
+    since = None
+    until = None
+
     for consumerKey, consumerSecret, accessToken, accessTokenSecret in keys:
         api = get_api(consumerKey, consumerSecret, accessToken, accessTokenSecret)
-        tweets = get_data(api, "btc", 500)
+        if isTimeRange:
+            tweets = get_data_range(api, keyword, since, until, N)
+        else:
+            tweets = get_data(api, keyword, N)
         save_df(tweets)
 
 
